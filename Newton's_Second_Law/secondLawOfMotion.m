183c487c4e4366182283da8e482706244889792b
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2022-09-16 07:14:25 +0500

    modified momentum formulae

---
+++ Newton's_Second_Law/secondLawOfMotion.m
@@ -0,0 +1,25 @@
+%Second law of motion in MATLAB!
+
+
+m1=100; %Mass is in Kg
+a1=2.4;%m/s^2
+
+netForce=m1*a1
+
+m2=100; %Mass is in Kg
+a2=2.4;%m/s^2
+
+changeInForce=m1*m2-a1*a2
+
+
+%if acceleration of this particular body is 2.4ms/s*2 then we assume that
+%the velocity is 2ms*-1 so momentum can be
+v=2
+momentum= m1*v
+
+
+Mass=[40]; %Mass is in Kg
+Acceleration=[2,2.4];%m/s^2 %where first parameter is the initial acceleration and 2nd index of array is af
+
+
+Force = Mass .* Acceleration
