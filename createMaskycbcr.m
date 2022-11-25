ba97d88169921843e86a0f37ab7099d236f3b022
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2022-11-25 09:22:52 +0500

    masking using YbCbCr

---
+++ createMaskycbcr.m
@@ -0,0 +1,28 @@
+function [BW,maskedRGBImage] = createMask(RGB)
+I = rgb2hsv(RGB);
+
+% Define thresholds for channel 1 based on histogram settings
+channel1Min = 0.523;
+channel1Max = 0.034;
+
+% Define thresholds for channel 2 based on histogram settings
+channel2Min = 0.085;
+channel2Max = 1.000;
+
+% Define thresholds for channel 3 based on histogram settings
+channel3Min = 0.742;
+channel3Max = 1.000;
+
+% Create mask based on chosen histogram thresholds
+sliderBW = ( (I(:,:,1) >= channel1Min) | (I(:,:,1) <= channel1Max) ) & ...
+    (I(:,:,2) >= channel2Min ) & (I(:,:,2) <= channel2Max) & ...
+    (I(:,:,3) >= channel3Min ) & (I(:,:,3) <= channel3Max);
+BW = sliderBW;
+
+% Initialize output masked image based on input image.
+maskedRGBImage = RGB;
+
+% Set background pixels where BW is false to zero.
+maskedRGBImage(repmat(~BW,[1 1 3])) = 0;
+
+end
