83aa77fe3f08fca73dc6fbbc04bab2f01b50945b
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2022-12-13 13:49:53 +0500

    numericalAnalysisFinal

---
+++ labEval.m
@@ -0,0 +1,27 @@
+%Solve the differential Equation
+%dy/dt=(1-t)*y where Initial condition y(0)=1 from t=0 to t=5   
+%the solution to the equation is y=(e^t)-(exp^t^2/2)
+%t=0:1:5;
+%eqn = diff(y,t,1) == (1-t)*y;
+%y=(exp(0))-(exp(0^2/2));
+%cond = y(0) ==1;
+%ySol(t) = dsolve(eqn,cond)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%Second Part Plotting 
