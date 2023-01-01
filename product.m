3916f8af8875a4be90b46daa3ab8c7cd4e6ca17b
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-01-01 11:29:58 +0500

    made the logic for product of two matrices

---
+++ product.m
@@ -0,0 +1,10 @@
+let a=rand(3,3);
+let b=input("Enter a matrix ");
+let multi=null;
+if(size(b)===size(a)){
+multi=a*b;
+}
+else{
+disp("The Matrix which you entered does not satisfy matrix multiplication order")
+}
+end
