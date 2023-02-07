86d39236fecac325b107924a5749393f16f0a3fd
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-02-08 01:13:26 +0500

    update

--- dcMotorSpeed.m
+++ dcMotorSpeed.m
@@ -1,1 +1,13 @@
 // To control the speed of the DC motor using arduino in MATLAB IDE
+ Speed control of DC motor
+a=arduino()
+for i=1:5;
+   
+writePWMVoltage(a,'D9',i);
+    pause(3)
+end
+   for i=5:-1:0;
+    writePWMVoltage(a,'D9',i);
+    pause(2)
+   end
+   
