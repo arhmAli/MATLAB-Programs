04b1186028b7237420905b682c3a7e680c679057
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-02-07 21:57:39 +0500

    made quadratic in C

---
+++ C-Programs/quadratic.c
@@ -0,0 +1,18 @@
+/* Include Files */
+#include "QuadraticEquation.h"
+
+/* Function Definitions */
+/*
+ * discriminant. formula
+ *
+ * Arguments    : double a
+ *                double b
+ *                double c
+ * Return Type  : double
+ */
+double QuadraticEquation(double a, double b, double c)
+{
+  return b * b - 4.0 * a * c;
+  /*  for unequal roots */
+}
+
