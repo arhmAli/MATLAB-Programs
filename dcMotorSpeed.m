ab85e1a9b1a3ecc09226e842fb13635f680c27da
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-02-08 01:15:32 +0500

    ft:added comments

--- dcMotorSpeed.m
+++ dcMotorSpeed.m
@@ -2,8 +2,9 @@
  Speed control of DC motor
 a=arduino()
 for i=1:5;
-   
-writePWMVoltage(a,'D9',i);
+   %As we know that the arduino has the fixed voltage pin of 5V so we vary the speed from 0 to 5 and check the speed
+    writePWMVoltage(a,'D9',i);
+    % 3 seconds pause to ensure that we take out time to notice the diff as each 1V diff is added.
     pause(3)
 end
    for i=5:-1:0;
