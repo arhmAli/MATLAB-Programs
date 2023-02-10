cfcb2adbf399f5f020791295a60a1bc4ec7943fa
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-02-10 08:21:22 +0500

    avg in m

---
+++ avg.m
@@ -0,0 +1,21 @@
+%Initialize an array of numbers
+x=[150 231 630 901 402 345 660 890 200 101];
+%Reference creation for the sum of the array.
+sum=0;
+%Global Reference creation for the average of the array 
+average=0;
+%mean
+meanOf=mean(x);
+%median
+medianOf=median(x);
+%Loop initialization
+for i=1:1:length(x)
+    %everytime the loop executes sum variable is updated
+    sum=sum+x(i);
+    %average is also updated on each iteration
+    average=sum/length(x);
+end
+%At the end of the program we display the result.
+disp(average);
+disp(meanOf);
+disp(medianOf);
