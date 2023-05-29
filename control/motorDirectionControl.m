d6d9731e22b15e2a850c6455dcf9d9b7b26a1991
Arham Khan <arhamkhank353@gmail.com>
2023-05-29 14:59:10 +0500

    direction control

---
+++ control/motorDirectionControl.m
@@ -0,0 +1,9 @@
+%direction control
+a=arduino()
+for i=1:1;
+    writeDigitalPin(a,'D6',0);
+    writeDigitalPin(a,'D9',1);
+    pause(5)
+    writeDigitalPin(a,'D6',1);
+    writeDigitalPin(a,'D9',0);
+end
